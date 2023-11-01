5e56983902f9a9a0d14d0edd87e42c43571c29bd
SimonOkello <simonokello93@gmail.com>
2023-11-02 02:39:11 +0300

    add django container

---
+++ command.md
@@ -0,0 +1,5 @@
+### Commands
+- python3 -m venv venv
+  
+- pip install Django celery redis
+
---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/ash
+
+echo "Apply database migration"
+
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000
